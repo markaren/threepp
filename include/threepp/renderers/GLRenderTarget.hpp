3e83311a5fb105f17e892deaef073e4775090bdd
Lars Ivar Hatledal <laht@ntnu.no>
2021-06-25 11:02:24 +0200

    update

--- include/threepp/renderers/GLRenderTarget.hpp
+++ include/threepp/renderers/GLRenderTarget.hpp
@@ -9,6 +9,8 @@
 
 #include "threepp/math/Vector4.hpp"
 
+#include <optional>
+
 namespace threepp {
 
     class GLRenderTarget: public EventDispatcher {
@@ -19,17 +21,23 @@ namespace threepp {
             int wrapS;
             int wrapT;
             int magFilter;
-            int minFilter;
+            int minFilter = LinearFilter;
             int type;
             int anisotropy;
             int encoding;
+
+            bool generateMipmaps = false;
+            bool depthBuffer = true;
+            bool stencilBuffer = false;
+            std::optional<Texture> depthTexture;
         };
 
     public:
         GLRenderTarget(unsigned int width, unsigned int height, const Options &options)
             : width_(width), height_(height),
-              scissor(0, 0, (float) width, (float) height),
-              viewPort(0, 0, (float) width, (float) height),
+              scissor_(0, 0, (float) width, (float) height),
+              viewPort_(0, 0, (float) width, (float) height),
+              depthBuffer_(options.depthBuffer), stencilBuffer_(options.stencilBuffer), depthTexture_(options.depthTexture),
               texture_(std::nullopt, options.mapping, options.wrapS, options.wrapT, options.magFilter, options.minFilter, options.type, options.anisotropy, options.encoding) {
         }
 
@@ -56,9 +64,33 @@ namespace threepp {
 
             }
 
-            this->viewport.set( 0, 0, width, height );
-            this->scissor.set( 0, 0, width, height );
+            this->viewport_.set( 0, 0, (float) width, (float) height );
+            this->scissor_.set( 0, 0, (float) width, (float) height );
 
+        }
+
+        GLRenderTarget &copy( const GLRenderTarget &source ) {
+
+                this->width = source.width;
+                this->height = source.height;
+                this->depth = source.depth;
+
+                this->viewport_.copy( source.viewport );
+
+                this->texture_ = source.texture;
+//                this->texture_.image = { ...this->texture.image }; // See #20328.
+
+                this->depthBuffer_ = source.depthBuffer;
+                this->stencilBuffer_ = source.stencilBuffer;
+                this->depthTexture_ = source.depthTexture;
+
+                return this;
+
+        }
+        
+        void dispose() {
+            
+            this->dispatchEvent("dispose");
         }
 
     private:
@@ -66,12 +98,16 @@ namespace threepp {
         const unsigned int height_;
         const unsigned int depth_ = 1;
 
-        Vector4 scissor;
-        bool scissorTest = false;
+        Vector4 scissor_;
+        bool scissorTest_ = false;
 
-        Vector4 viewPort;
+        Vector4 viewPort_;
 
         Texture texture_;
+
+        bool depthBuffer_;
+        bool stencilBuffer_;
+        std::optional<Texture> depthTexture_;
     };
 
 }// namespace threepp
