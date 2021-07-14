a528b5ccb937d4fdbf577da8ce2080d4a4530a6d
Lars Ivar Hatledal <laht@hials.no>
2021-07-14 17:39:48 +0200

    update

--- include/threepp/input/MouseListener.hpp
+++ include/threepp/input/MouseListener.hpp
@@ -1,6 +1,6 @@
 
-#ifndef THREEPP_KEYLISTENER_HPP
-#define THREEPP_KEYLISTENER_HPP
+#ifndef THREEPP_MOUSELISTENER_HPP
+#define THREEPP_MOUSELISTENER_HPP
 
 #include <functional>
 #include <utility>
@@ -17,39 +17,7 @@ namespace threepp {
             : button(button), action(action), mods(mods) {}
     };
 
-    struct KeyEvent {
-
-        const int key;
-        const int scancode;
-        const int mods;
-
-        KeyEvent(int key, int scancode, int mods)
-            : key(key), scancode(scancode), mods(mods) {}
-    };
-
-    struct KeyListener {
-
-        virtual void onKeyPressed(KeyEvent evt) {}
-
-        virtual void onKeyReleased(KeyEvent evt) {}
-
-        virtual void onKeyRepeat(KeyEvent evt) {}
-    };
-
-    struct KeyPressAdapter : KeyListener {
-
-        explicit KeyPressAdapter(std::function<void(KeyEvent)> f)
-            : f(std::move(f)) {}
-
-        void onKeyPressed(KeyEvent evt) override {
-            f(evt);
-        }
-
-    private:
-        std::function<void(KeyEvent)> f;
-    };
-
 }// namespace threepp
 
 
-#endif//THREEPP_KEYLISTENER_HPP
+#endif//THREEPP_MOUSELISTENER_HPP
