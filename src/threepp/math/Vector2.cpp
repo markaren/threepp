3b173ff70c89c3600c261d297de37ff8b7b4ca3a
Lars Ivar Hatledal <laht@hials.no>
2021-07-21 22:48:33 +0200

    fix

--- src/threepp/math/Vector2.cpp
+++ src/threepp/math/Vector2.cpp
@@ -267,7 +267,7 @@ float Vector2::manhattanLength() const {
 Vector2 &Vector2::normalize() {
 
     const auto len = this->length();
-    return this->divideScalar(isnan(len) ? 1 : len );
+    return this->divideScalar(std::isnan(len) ? 1 : len );
 
 }
 
