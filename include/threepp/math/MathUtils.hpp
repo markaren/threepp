5b7473d2adfa6b7c9c66cafe403f80474b5b7ab2
Lars Ivar Hatledal <laht@ntnu.no>
2022-11-11 08:26:47 +0100

    add math functions

--- include/threepp/math/MathUtils.hpp
+++ include/threepp/math/MathUtils.hpp
@@ -2,6 +2,7 @@
 #ifndef THREEPP_MATHUTILS_HPP
 #define THREEPP_MATHUTILS_HPP
 
+#include <algorithm>
 #include <cmath>
 
 namespace threepp::math {
@@ -11,6 +12,24 @@ namespace threepp::math {
 
     const float DEG2RAD = PI / 180.f;
     const float RAD2DEG = 180.f / PI;
+
+    template<class T>
+    T clamp(T value, T min, T max) {
+
+        return std::max(min, std::min(max, value));
+    }
+
+    template<class T>
+    void clamp(T& value, T min, T max) {
+
+        value = std::max(min, std::min(max, value));
+    }
+
+    float mapLinear(float x, float a1, float a2, float b1, float b2);
+
+    float inverseLerp(float x, float y, float value);
+
+    float lerp(float x, float y, float t);
 
     float degToRad(float degrees);
 
