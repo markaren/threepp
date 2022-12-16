3a80fc7c0108e12ec1dc46a90704a5d816f39f92
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2022-12-17 00:08:27 +0100

    update

--- examples/projects/Crane3R/main.cpp
+++ examples/projects/Crane3R/main.cpp
@@ -1,7 +1,9 @@
 
 #include "threepp/threepp.hpp"
 
-#include <future>
+#include <thread>
+
+using namespace threepp;
 
 #ifdef HAS_IMGUI
 #include "../../imgui_helper.hpp"
@@ -29,61 +31,75 @@ struct MyUI : public imggui_helper {
 };
 #endif
 
+class Crane3R : public Group {
 
-using namespace threepp;
+public:
 
-std::shared_ptr<Object3D> loadCrane() {
+    void setAngles(float j1, float j2, float j3) {
+        p1_->rotation.z = math::DEG2RAD * j1;
+        p2_->rotation.y = math::DEG2RAD * j2;
+        p3_->rotation.y = math::DEG2RAD * j3;
+    }
 
-    OBJLoader loader;
-    auto part1 = loader.load("data/models/obj/Crane3R/4200/4200.obj");
-    auto part2 = loader.load("data/models/obj/Crane3R/7000/7000.obj");
-    part2->position.set(0, 0, 4.2);
-    part1->add(part2);
-    auto part3 = loader.load("data/models/obj/Crane3R/5200/5200.obj");
-    part3->position.set(7, 0, 0);
-    part2->add(part3);
+    static std::shared_ptr<Crane3R> create() {
 
-    part1->rotateX(-math::PI / 2);
+        OBJLoader loader;
+        auto part1 = loader.load("data/models/obj/Crane3R/4200/4200.obj");
+        auto part2 = loader.load("data/models/obj/Crane3R/7000/7000.obj");
+        part2->position.set(0, 0, 4.2);
+        part1->add(part2);
+        auto part3 = loader.load("data/models/obj/Crane3R/5200/5200.obj");
+        part3->position.set(7, 0, 0);
+        part2->add(part3);
 
-    part1->name = "part1";
-    part2->name = "part2";
-    part3->name = "part3";
+        part1->rotateX(-math::PI / 2);
 
-    return part1;
-}
+        part1->name = "part1";
+        part2->name = "part2";
+        part3->name = "part3";
+
+        return std::shared_ptr<Crane3R>(new Crane3R(part1));
+    }
+
+private:
+    Object3D* p1_;
+    Object3D* p2_;
+    Object3D* p3_;
+
+    explicit Crane3R(const std::shared_ptr<Group> &obj) {
+
+        p1_ = obj->getObjectByName("part1");
+        p2_ = obj->getObjectByName("part2");
+        p3_ = obj->getObjectByName("part3");
+
+        add(obj);
+    }
+};
 
 int main() {
 
-    Canvas canvas{Canvas::Parameters().antialiasing(8)};
+    Canvas canvas{Canvas::Parameters().size({1280, 720}).antialiasing(8)};
     GLRenderer renderer{canvas};
     renderer.setClearColor(Color::aliceblue);
 
-    auto camera = PerspectiveCamera::create();
+    auto camera = PerspectiveCamera::create(60, canvas.getAspect());
     camera->position.set(15, 8, 15);
 
     OrbitControls controls(camera, canvas);
 
     auto scene = Scene::create();
 
-    auto grid = GridHelper::create(20);
+    auto grid = GridHelper::create(20, 10, Color::yellowgreen);
     scene->add(grid);
 
     auto light = AmbientLight::create(Color::white);
     scene->add(light);
 
-    bool loaded = false;
-
-    Object3D *part1;
-    Object3D *part2;
-    Object3D *part3;
+    std::shared_ptr<Crane3R> crane;
     std::thread t([&] {
-        auto crane = loadCrane();
-        part1 = crane->getObjectByName("part1");
-        part2 = crane->getObjectByName("part2");
-        part3 = crane->getObjectByName("part3");
+        crane = Crane3R::create();
         canvas.invokeLater([&, crane] {
             scene->add(crane);
-            loaded = true;
         });
     });
 
@@ -101,13 +117,11 @@ int main() {
 
 #ifdef HAS_IMGUI
 
-        if (loaded) {
+        if (crane) {
             ui.render();
 
             auto &angles = ui.angles;
-            part1->rotation.z = math::DEG2RAD*angles[0];
-            part2->rotation.y = math::DEG2RAD*angles[1];
-            part3->rotation.y = math::DEG2RAD*angles[2];
+            crane->setAngles(angles[0], angles[1], angles[2]);
         }
 
         controls.enabled = !ui.mouseHover;
