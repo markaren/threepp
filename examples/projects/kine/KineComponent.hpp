30cd93516c0769082589f57180281494a5976b3f
Lars Ivar Hatledal <laht@ntnu.no>
2022-12-21 08:48:02 +0100

    remove unused code

--- examples/projects/kine/KineComponent.hpp
+++ examples/projects/kine/KineComponent.hpp
@@ -11,31 +11,8 @@ namespace kine {
     class KineComponent {
 
     public:
-        [[nodiscard]] bool isFirst() const {
-            return previous_ == nullptr;
-        }
-
-        [[nodiscard]] bool isLast() const {
-            return next_ == nullptr;
-        }
-
-        void setPrevious(KineComponent* c) {
-            previous_ = c;
-        }
-
-        void setNext(KineComponent* c) {
-            next_ = c;
-        }
-
-        KineComponent* next() {
-            return next_;
-        }
 
         [[nodiscard]] virtual threepp::Matrix4 getTransformation() const = 0;
-
-    private:
-        KineComponent* previous_ = nullptr;
-        KineComponent* next_ = nullptr;
 
     };
 
