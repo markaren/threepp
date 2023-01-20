a3961598b2eb88908f633cb3ce00ce5d2a933c08
Lars Ivar Hatledal <laht@ntnu.no>
2023-01-20 13:21:46 +0100

    update vcpkg registry demo [ci skip]

--- tests/threepp_vcpkg_test/main.cpp
+++ tests/threepp_vcpkg_test/main.cpp
@@ -1,7 +1,10 @@
 
 #include "threepp/threepp.hpp"
 
+#include "threepp/extras/imgui/imgui_context.hpp"
+
 using namespace threepp;
+
 
 int main() {
 
@@ -23,9 +26,27 @@ int main() {
 
     scene->add(mesh);
 
+#ifdef HAS_IMGUI
+    std::array<float, 3> posBuf{};
+    imgui_functional_context ui(canvas.window_ptr(), [&]{
+        ImGui::SetNextWindowPos({0, 0}, 0, {0,0});
+        ImGui::SetNextWindowSize({230, 0}, 0);
+        ImGui::Begin("Demo");
+        ImGui::SliderFloat3("position", posBuf.data(), -1.f, 1.f);
+        controls.enabled = !ImGui::IsWindowHovered();
+        ImGui::End();
+    });
+#endif
+
     canvas.animate([&]{
 
         renderer.render(scene, camera);
+        ui.render();
+
+#ifdef HAS_IMGUI
+        mesh->position.fromArray(posBuf);
+#endif
+
     });
 
 }
