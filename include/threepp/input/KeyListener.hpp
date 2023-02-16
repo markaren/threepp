dfc5de40254f6c5580f7bedaa2a7bcbf9d116464
Lars Ivar Hatledal <laht@ntnu.no>
2023-02-16 11:25:02 +0100

    accept flags in keyadapter

--- include/threepp/input/KeyListener.hpp
+++ include/threepp/input/KeyListener.hpp
@@ -35,31 +35,38 @@ namespace threepp {
     struct KeyAdapter: KeyListener {
 
         enum Mode {
-            KEY_PRESSED,
-            KEY_RELEASED,
-            KEY_REPEAT
+            KEY_PRESSED = 1,
+            KEY_RELEASED = 2,
+            KEY_REPEAT = 4
         };
 
         KeyAdapter(const Mode &mode, std::function<void(KeyEvent)> f)
             : mode_(mode), f_(std::move(f)) {}
 
         void onKeyPressed(KeyEvent evt) override {
-            if (mode_ == KEY_PRESSED) f_(evt);
+            if (mode_ == 1 || mode_ == 3 || mode_ == 5) f_(evt);
         }
 
         void onKeyReleased(KeyEvent evt) override {
-            if (mode_ == KEY_RELEASED) f_(evt);
+            if (mode_ == 2 || mode_ == 3 || mode_ == 6) f_(evt);
         }
 
         void onKeyRepeat(KeyEvent evt) override {
-            if (mode_ == KEY_REPEAT) f_(evt);
+            if (mode_ == 4 || mode_ == 5 || mode_ == 6) f_(evt);
         }
 
     private:
-        const Mode mode_;
+        Mode mode_;
         std::function<void(KeyEvent)> f_;
 
     };
+
+
+    inline KeyAdapter::Mode operator|(KeyAdapter::Mode a, KeyAdapter::Mode b)
+    {
+        return static_cast<KeyAdapter::Mode>(static_cast<int>(a) | static_cast<int>(b));
+    }
+
 
 }// namespace threepp
 
