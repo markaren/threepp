f34569bb88e00881735ea190c2f768a17cc55f00
Lars Ivar Hatledal <laht@ntnu.no>
2023-02-17 12:16:11 +0100

    use re2

--- src/threepp/loaders/TextureLoader.cpp
+++ src/threepp/loaders/TextureLoader.cpp
@@ -1,13 +1,14 @@
 
 #include "threepp/loaders/TextureLoader.hpp"
 
-#include <regex>
+#include <re2/re2.h>
 
 using namespace threepp;
 
 std::shared_ptr<Texture> TextureLoader::loadTexture(const std::filesystem::path& path, bool flipY) {
 
-    bool isJPEG = std::regex_match(path.string(), std::regex(".*jpe?g", std::regex::icase));
+    static RE2 r("(?i).*jpe?g");
+    bool isJPEG = RE2::PartialMatch(re2::StringPiece(path.string()), r);
 
     auto image = imageLoader_.load(path, isJPEG ? 3 : 4, flipY);
 
