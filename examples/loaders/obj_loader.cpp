f763c8a0ab38d630e62fb9196234330978f1c1dc
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2023-02-26 15:02:57 +0100

    merge obj examples

--- examples/loaders/obj_loader.cpp
+++ examples/loaders/obj_loader.cpp
@@ -12,27 +12,29 @@ int main() {
     auto camera = PerspectiveCamera::create(75, canvas.getAspect(), 0.1f, 1000);
     camera->position.set(0, 100, 150);
 
+    OBJLoader loader;
+    auto obj1 = loader.load("data/models/obj/female02/female02.obj");
+    obj1->position.x = -30;
+    scene->add(obj1);
+
     TextureLoader tl;
     auto tex = tl.loadTexture("data/textures/uv_grid_opengl.jpg");
-
-    OBJLoader loader;
-    auto obj = loader.load("data/models/obj/female02/female02.obj", false);
-    obj->traverseType<Mesh>([tex](Mesh &child) {
-        for (auto& m : child.materials()) {
-            auto materialWithMap = m->as<MaterialWithMap>();
-            if (materialWithMap) {
-                materialWithMap->map = tex;
-            }
-        }
+    auto obj2 = loader.load("data/models/obj/female02/female02.obj", false);
+    obj2->materials().clear();
+    obj2->position.x = 30;
+    obj2->traverseType<Mesh>([tex](Mesh& child) {
+        auto m = MeshPhongMaterial::create();
+        m->map = tex;
+        child.setMaterial(m);
     });
-    scene->add(obj);
+    scene->add(obj2);
 
     auto light1 = PointLight::create(0xffffff, 1.f);
-    light1->position.set(25, 115, 25);
+    light1->position.set(45, 115, 25);
     scene->add(light1);
 
     auto light2 = PointLight::create(0xffffff, 1.f);
-    light2->position.set(-25, 115, 125);
+    light2->position.set(-45, 115, 125);
     scene->add(light2);
 
     auto light3 = PointLight::create(0xffffff, 1.f);
@@ -46,7 +48,8 @@ int main() {
     });
 
     canvas.animate([&](float dt) {
-        obj->rotation.y += 1 * dt;
+        obj1->rotation.y += 1 * dt;
+        obj2->rotation.y += 1 * dt;
 
         renderer.render(scene, camera);
     });
