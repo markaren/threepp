ff23ec52644a7f137ce0717315013c995f7f3074
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2023-03-03 09:48:14 +0100

    add operator == and fix ostream output

--- include/threepp/math/Box3.hpp
+++ include/threepp/math/Box3.hpp
@@ -131,8 +131,13 @@ namespace threepp {
 
         [[nodiscard]] bool equals(const Box3& box) const;
 
+        bool operator==(const Box3& other) const {
+
+            return equals(other);
+        }
+
         friend std::ostream& operator<<(std::ostream& os, const Box3& v) {
-            os << "Box3(max=" << v.min_ << ", max=" << v.max_ << ")";
+            os << "Box3(min=" << v.min_ << ", max=" << v.max_ << ")";
             return os;
         }
 
