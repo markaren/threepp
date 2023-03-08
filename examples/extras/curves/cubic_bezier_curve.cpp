dbd6543e794df8676845269b8c534535b8205795
Lars Ivar Hatledal <laht@ntnu.no>
2023-03-08 15:03:52 +0100

    Add CubicBezier

---
+++ examples/extras/curves/cubic_bezier_curve.cpp
@@ -0,0 +1,84 @@
+
+#include "threepp/extras/curves/CubicBezierCurve.hpp"
+#include "threepp/extras/curves/CubicBezierCurve3.hpp"
+#include "threepp/threepp.hpp"
+
+using namespace threepp;
+
+int main() {
+
+    Canvas canvas(Canvas::Parameters().antialiasing(8));
+    GLRenderer renderer(canvas);
+    renderer.setClearColor(Color::aliceblue);
+
+    auto scene = Scene::create();
+    auto camera = PerspectiveCamera::create(60, canvas.getAspect(), 0.1f, 100);
+    camera->position.set(0, 5, 20);
+
+    OrbitControls controls{camera, canvas};
+
+    const auto material = LineBasicMaterial::create();
+    material->color = 0xff0000;
+
+    std::vector<Vector2> curve2Points;
+    std::vector<Vector3> curve3Points;
+
+    //2D
+    {
+        auto curve = CubicBezierCurve::create(
+                Vector2(-10, 0),
+                Vector2(-5, 15),
+                Vector2(20, 15),
+                Vector2(10, 0));
+
+        curve2Points = curve->getPoints(50);
+        const auto geometry = BufferGeometry::create();
+        geometry->setFromPoints(curve2Points);
+        const auto curveObject = Line::create(geometry, material);
+        scene->add(curveObject);
+
+    }
+
+    //3D
+    {
+        auto curve = CubicBezierCurve3::create(
+                Vector3(-10, 0, 0),
+                Vector3(-5, 5, -15),
+                Vector3(20, 5, -15),
+                Vector3(10, 0, 0));
+
+        curve3Points = curve->getPoints(50);
+        const auto geometry = BufferGeometry::create();
+        geometry->setFromPoints(curve3Points);
+
+        const auto curveObject = Line::create(geometry, material);
+        scene->add(curveObject);
+
+    }
+
+    Matrix4 tmp;
+    auto spheresMaterial = MeshBasicMaterial::create();
+    spheresMaterial->color = Color::black;
+    auto spheres = InstancedMesh::create(SphereGeometry::create(0.075f), spheresMaterial, curve2Points.size() + curve3Points.size());
+    int index = 0;
+    for (const auto& p : curve2Points) {
+        tmp.setPosition(p.x, p.y, 0);
+        spheres->setMatrixAt(index++, tmp);
+    }
+    for (const auto& p : curve3Points) {
+        tmp.setPosition(p.x, p.y, p.z);
+        spheres->setMatrixAt(index++, tmp);
+    }
+    scene->add(spheres);
+
+
+    canvas.onWindowResize([&](WindowSize size) {
+        camera->aspect = size.getAspect();
+        camera->updateProjectionMatrix();
+        renderer.setSize(size);
+    });
+
+    canvas.animate([&](float dt) {
+        renderer.render(scene, camera);
+    });
+}
---
+++ include/threepp/extras/curves/CubicBezierCurve3.hpp
@@ -0,0 +1,41 @@
+// https://github.com/mrdoob/three.js/blob/r129/src/extras/curves/CubicBezierCurve.js
+
+#ifndef THREEPP_CUBICBEZIERCURVE3_HPP
+#define THREEPP_CUBICBEZIERCURVE3_HPP
+
+#include "threepp/extras/core/Curve.hpp"
+#include "threepp/extras/core/Interpolations.hpp"
+
+#include <memory>
+
+namespace threepp {
+
+    class CubicBezierCurve3: public Curve3 {
+
+    public:
+        explicit CubicBezierCurve3(const Vector3& v0 = {}, const Vector3& v1 = {}, const Vector3& v2 = {}, const Vector3& v3 = {})
+            : v0(v0), v1(v1), v2(v2), v3(v3) {}
+
+        void getPoint(float t, Vector3& point) override {
+
+            point.set(
+                    CubicBezier(t, v0.x, v1.x, v2.x, v3.x),
+                    CubicBezier(t, v0.y, v1.y, v2.y, v3.y),
+                    CubicBezier(t, v0.z, v1.z, v2.z, v3.z));
+        }
+
+        static std::shared_ptr<CubicBezierCurve3> create(const Vector3& v0 = {}, const Vector3& v1 = {}, const Vector3& v2 = {}, const Vector3& v3 = {}) {
+
+            return std::make_shared<CubicBezierCurve3>(v0, v1, v2, v3);
+        }
+
+    private:
+        Vector3 v0;
+        Vector3 v1;
+        Vector3 v2;
+        Vector3 v3;
+    };
+
+}// namespace threepp
+
+#endif//THREEPP_CUBICBEZIERCURVE3_HPP
