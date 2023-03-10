9d222bfbcacaad66dbfef5ec589e1fb33017f663
Lars Ivar Hatledal <laht@ntnu.no>
2023-03-10 14:01:23 +0100

    add comment

--- include/threepp/math/MathUtils.hpp
+++ include/threepp/math/MathUtils.hpp
@@ -40,6 +40,7 @@ namespace threepp::math {
 
     float floorPowerOfTwo(float value);
 
+    //https://stackoverflow.com/questions/1903954/is-there-a-standard-sign-function-signum-sgn-in-c-c
     template<typename T>
     inline int sgn(T val) {
         return (T(0) < val) - (val < T(0));
