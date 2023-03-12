516baa8ba9edbcc1cd442b819012ef8d6cb94962
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2023-03-12 21:55:58 +0100

    update plane

--- src/threepp/math/Plane.cpp
+++ src/threepp/math/Plane.cpp
@@ -36,8 +36,8 @@ Plane& Plane::setFromNormalAndCoplanarPoint(const Vector3& normal, const Vector3
 
 Plane& Plane::setFromCoplanarPoints(const Vector3& a, const Vector3& b, const Vector3& c) {
 
-    Vector3 _vector1{};
-    Vector3 _vector2{};
+    Vector3 _vector1;
+    Vector3 _vector2;
     const auto& normal = _vector1.subVectors(c, b).cross(_vector2.subVectors(a, b)).normalize();
 
     // Q: should an error be thrown if normal is zero (e.g. degenerate plane)?
@@ -91,7 +91,7 @@ void Plane::projectPoint(const Vector3& point, Vector3& target) const {
 
 void Plane::intersectLine(const Line3& line, Vector3& target) const {
 
-    Vector3 _vector1{};
+    Vector3 _vector1;
     line.delta(_vector1);
     const auto& direction = _vector1;
 
@@ -100,31 +100,31 @@ void Plane::intersectLine(const Line3& line, Vector3& target) const {
     if (denominator == 0) {
 
         // line is coplanar, return origin
-        if (this->distanceToPoint(line.getStart()) == 0) {
+        if (this->distanceToPoint(line.start()) == 0) {
 
-            target.copy(line.getEnd());
+            target.copy(line.end());
         }
 
         // Unsure if this is the correct method to handle this case.
         return;
     }
 
-    const auto t = -(line.getStart().dot(this->normal) + this->constant) / denominator;
+    const auto t = -(line.start().dot(this->normal) + this->constant) / denominator;
 
     if (t < 0 || t > 1) {
 
         return;
     }
 
-    target.copy(direction).multiplyScalar(t).add(line.getStart());
+    target.copy(direction).multiplyScalar(t).add(line.start());
 }
 
 bool Plane::intersectsLine(const Line3& line) const {
 
     // Note: this tests if a line intersects the plane, not whether it (or its end-points) are coplanar with it.
 
-    const auto startSign = this->distanceToPoint(line.getStart());
-    const auto endSign = this->distanceToPoint(line.getEnd());
+    const auto startSign = this->distanceToPoint(line.start());
+    const auto endSign = this->distanceToPoint(line.end());
 
     return (startSign < 0 && endSign > 0) || (endSign < 0 && startSign > 0);
 }
@@ -146,15 +146,15 @@ void Plane::coplanarPoint(Vector3& target) const {
 
 Plane& Plane::applyMatrix4(const Matrix4& matrix) {
 
-    Vector3 _vector1{};
-    Matrix3 _normalMatrix{};
+    Vector3 _vector1;
+    Matrix3 _normalMatrix;
 
-    const auto normalMatrix = _normalMatrix.getNormalMatrix(matrix);
+    const auto& normalMatrix = _normalMatrix.getNormalMatrix(matrix);
 
     this->coplanarPoint(_vector1);
-    const auto referencePoint = _vector1.applyMatrix4(matrix);
+    const auto& referencePoint = _vector1.applyMatrix4(matrix);
 
-    const auto normal = this->normal.applyMatrix3(normalMatrix).normalize();
+    const auto& normal = this->normal.applyMatrix3(normalMatrix).normalize();
 
     this->constant = -referencePoint.dot(normal);
 
@@ -163,12 +163,12 @@ Plane& Plane::applyMatrix4(const Matrix4& matrix) {
 
 Plane& Plane::applyMatrix4(const Matrix4& matrix, Matrix3& normalMatrix) {
 
-    Vector3 _vector1{};
+    Vector3 _vector1;
 
     this->coplanarPoint(_vector1);
-    const auto referencePoint = _vector1.applyMatrix4(matrix);
+    const auto& referencePoint = _vector1.applyMatrix4(matrix);
 
-    const auto normal = this->normal.applyMatrix3(normalMatrix).normalize();
+    const auto& normal = this->normal.applyMatrix3(normalMatrix).normalize();
 
     this->constant = -referencePoint.dot(normal);
 
