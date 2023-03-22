3022e54ed8302100bacd96c13751d470fab31648
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2023-03-22 20:20:04 +0100

    update

--- examples/extras/physics/agx/test.cpp
+++ examples/extras/physics/agx/test.cpp
@@ -8,9 +8,23 @@ using namespace threepp;
 
 namespace {
 
+    inline Vector3 randomPosition() {
+        return {math::randomInRange(-5.f, 5.f), math::randomInRange(5.f, 15.f), math::randomInRange(-5.f, 5.f)};
+    }
+
+    void initPositions(InstancedMesh& m) {
+        Matrix4 matrix;
+        for (unsigned i = 0; i < m.count; i++) {
+
+            matrix.setPosition(randomPosition());
+            m.setMatrixAt(i, matrix);
+            m.setColorAt(i, Color().randomize());
+        }
+    }
+
     auto createSphere() {
 
-        const auto geometry = SphereGeometry::create(0.1f);
+        const auto geometry = SphereGeometry::create(0.5f);
         const auto material = MeshLambertMaterial::create();
         material->color.setHex(0xff0000);
         auto mesh = Mesh::create(geometry, material);
@@ -19,12 +33,17 @@ namespace {
 
     auto createPlane() {
 
-        const auto geometry = PlaneGeometry::create(10, 10);
-        geometry->rotateX(-math::PI / 2);
+        const auto geometry = BoxGeometry::create(10, 1, 10);
         const auto material = MeshLambertMaterial::create();
         material->color = Color::gray;
         auto mesh = Mesh::create(geometry, material);
         return mesh;
+    }
+
+    auto createBoxes(unsigned int count) {
+        const auto geometry = BoxGeometry::create(0.5f, 0.5f, 0.5f);
+        const auto material = MeshPhongMaterial::create();
+        return InstancedMesh::create(geometry, material, count);
     }
 
 }// namespace
@@ -47,8 +66,13 @@ int main() {
     sphere->position.y = 10;
     auto plane = createPlane();
 
+    auto boxes = createBoxes(20);
+    boxes->instanceMatrix->setUsage(DynamicDrawUsage);
+    initPositions(*boxes);
+
     scene->add(sphere);
     scene->add(plane);
+    scene->add(boxes);
 
     canvas.onWindowResize([&](WindowSize size) {
         camera->aspect = size.getAspect();
@@ -59,6 +83,9 @@ int main() {
     AgxPhysics agx;
     agx.addMesh(*sphere, 1);
     agx.addMesh(*plane, 0);
+    agx.addMesh(*boxes, 10);
+
+    agx.saveScene("test");
 
     canvas.animate([&](float dt) {
         agx.step(dt);
--- include/threepp/extras/physics/AgxPhysics.hpp
+++ include/threepp/extras/physics/AgxPhysics.hpp
@@ -7,6 +7,7 @@
 #include <agxCollide/Geometry.h>
 #include <agxCollide/ShapePrimitives.h>
 #include <agxSDK/Simulation.h>
+#include <agxIO/ReaderWriter.h>
 
 #include <optional>
 
@@ -39,12 +40,12 @@ namespace threepp {
             } else if (geometry.type() == "BoxGeometry") {
 
                 auto g = dynamic_cast<BoxGeometry*>(&geometry);
-                return new agxCollide::Geometry(new agxCollide::Box(g->width, g->height, g->depth));
+                return new agxCollide::Geometry(new agxCollide::Box(g->width/2, g->height/2, g->depth/2));
 
             } else if (geometry.type() == "PlaneGeometry") {
 
                 auto g = dynamic_cast<PlaneGeometry*>(&geometry);
-                return new agxCollide::Geometry(new agxCollide::Box(g->width, 0.1f, g->height));
+                return new agxCollide::Geometry(new agxCollide::Box(g->width/2, 0.05f, g->height/2));
 
             } else if (geometry.type() == "CapsuleGeometry") {
 
@@ -54,6 +55,48 @@ namespace threepp {
             }
 
             return nullptr;
+        }
+
+        agx::RigidBodyRef createRigidBody(float mass, agxCollide::Geometry* geometry) {
+
+            agx::RigidBodyRef rb = new agx::RigidBody();
+            if (mass == 0) {
+                rb->setMotionControl(agx::RigidBody::STATIC);
+            } else {
+                rb->getMassProperties()->setMass(mass);
+            }
+            rb->add(geometry->clone());
+
+            return rb;
+        }
+
+        void compose(const agx::Vec3& position, const agx::Quat& quaternion, std::vector<float>& array, size_t index) {
+
+            auto x = quaternion.x(), y = quaternion.y(), z = quaternion.z(), w = quaternion.w();
+            auto x2 = x + x, y2 = y + y, z2 = z + z;
+            auto xx = x * x2, xy = x * y2, xz = x * z2;
+            auto yy = y * y2, yz = y * z2, zz = z * z2;
+            auto wx = w * x2, wy = w * y2, wz = w * z2;
+
+            array[index + 0] = (1 - (yy + zz));
+            array[index + 1] = (xy + wz);
+            array[index + 2] = (xz - wy);
+            array[index + 3] = 0;
+
+            array[index + 4] = (xy - wz);
+            array[index + 5] = (1 - (xx + zz));
+            array[index + 6] = (yz + wx);
+            array[index + 7] = 0;
+
+            array[index + 8] = (xz + wy);
+            array[index + 9] = (yz - wx);
+            array[index + 10] = (1 - (xx + yy));
+            array[index + 11] = 0;
+
+            array[index + 12] = position.x();
+            array[index + 13] = position.y();
+            array[index + 14] = position.z();
+            array[index + 15] = 1;
         }
 
 
@@ -88,13 +131,38 @@ namespace threepp {
 
                 if (mesh.is<InstancedMesh>()) {
 
-//                    handleInstancedMesh(mesh.as<InstancedMesh>(), mass, std::move(shape));
+                    handleInstancedMesh(mesh.as<InstancedMesh>(), mass, geometry);
 
                 } else if (mesh.is<Mesh>()) {
 
                     handleMesh(&mesh, mass, geometry);
                 }
             }
+        }
+
+        void setMeshPosition(Mesh& mesh, const Vector3& position, unsigned int index = 0) {
+
+            if (!meshMap.count(&mesh)) return;
+
+            auto& body = meshMap.at(&mesh);
+
+            body->setAngularVelocity({0,0,0});
+            body->setVelocity({0,0,0});
+
+            body->setPosition(toVec3(position));
+        }
+
+        void setInstancedMeshPosition(InstancedMesh& mesh, const Vector3& position, unsigned int index = 0) {
+
+            if (!instancedMeshMap.count(&mesh)) return;
+
+            auto& bodies = instancedMeshMap.at(&mesh);
+            auto& body = bodies[index];
+
+            body->setAngularVelocity({0,0,0});
+            body->setVelocity({0,0,0});
+
+            body->setPosition(toVec3(position));
         }
 
         void step(float dt) {
@@ -114,36 +182,70 @@ namespace threepp {
                     mesh->position.set(position.x(), position.y(), position.z());
                     mesh->quaternion.set(quaternion.x(), quaternion.y(), quaternion.z(), quaternion.w());
                 }
+
+                for (auto& [mesh, bodies] : instancedMeshMap) {
+
+                    auto& array = mesh->instanceMatrix->array();
+
+                    for (unsigned j = 0; j < bodies.size(); ++j) {
+
+                        auto& body = bodies[j];
+
+                        auto position = body->getPosition();
+                        auto quaternion = body->getRotation();
+
+                        detail::compose(position, quaternion, array, j * 16);
+                    }
+
+                    mesh->instanceMatrix->needsUpdate();
+                }
             }
+        }
+
+        void saveScene(const std::string& name) {
+
+            agxIO::writeFile(name + ".agx", sim);
         }
 
     private:
         float t = 0;
         agx::AutoInit init;
         agxSDK::SimulationRef sim = new agxSDK::Simulation();
-
-        std::unordered_map<Object3D*, agx::RigidBodyRef> meshMap;
-
+        std::unordered_map<Mesh*, agx::RigidBodyRef> meshMap;
+        std::unordered_map<InstancedMesh*, std::vector<agx::RigidBodyRef>> instancedMeshMap;
 
         void handleMesh(Mesh* mesh, float mass, agxCollide::Geometry* geometry) {
 
+            auto rb = detail::createRigidBody(mass, geometry);
             const auto& position = mesh->position;
             const auto& quaternion = mesh->quaternion;
 
-            agx::RigidBodyRef rb = new agx::RigidBody();
-            if (mass == 0) {
-                rb->setMotionControl(agx::RigidBody::STATIC);
-            } else {
-                rb->getMassProperties()->setMass(mass);
-            }
             rb->setPosition(toVec3(position));
             rb->setRotation(toQuat(quaternion));
-            rb->add(geometry);
             sim->add(rb);
 
             meshMap[mesh] = rb;
-
             mesh->addEventListener("remove", onMeshRemovedListener);
+        }
+
+        void handleInstancedMesh(InstancedMesh* mesh, float mass, agxCollide::Geometry* geometry) {
+
+            auto& array = mesh->instanceMatrix->array();
+
+            for (unsigned i = 0; i < mesh->count; i++) {
+
+                unsigned index = i * 16;
+                std::vector<double> slice{array.begin() + index, array.begin() + index + 16};
+
+                auto rb = detail::createRigidBody(mass, geometry);
+//                rb->setPosition({slice[13], slice[14], slice[15]});
+                rb->setLocalTransform(agx::AffineMatrix4x4().set(slice.data()));
+                sim->add(rb);
+
+                instancedMeshMap[mesh].emplace_back(rb);
+            }
+
+            mesh->addEventListener("remove", onInstancedMeshRemovedListener);
         }
 
         struct MeshRemovedListener: EventListener {
@@ -165,7 +267,27 @@ namespace threepp {
             AgxPhysics* scope;
         };
 
-        std::shared_ptr<MeshRemovedListener> onMeshRemovedListener = std::make_unique<MeshRemovedListener>(this);
+        struct InstancedMeshRemovedListener: EventListener {
+
+            explicit InstancedMeshRemovedListener(AgxPhysics* scope): scope(scope) {}
+
+            void onEvent(Event& event) override {
+                if (event.type == "remove") {
+                    auto m = static_cast<Mesh*>(event.target);
+                    if (scope->meshMap.count(m)) {
+                        auto rb = scope->meshMap.at(m);
+                        scope->sim->remove(rb);
+                        scope->meshMap.erase(m);
+                    }
+                }
+            }
+
+        private:
+            AgxPhysics* scope;
+        };
+
+        std::shared_ptr<EventListener> onMeshRemovedListener = std::make_unique<MeshRemovedListener>(this);
+        std::shared_ptr<EventListener> onInstancedMeshRemovedListener = std::make_unique<InstancedMeshRemovedListener>(this);
     };
 
 }// namespace threepp
