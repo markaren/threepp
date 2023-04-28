add45b79afbf8ab2b4e7179be93dd1dda7e9dc8d
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2023-04-28 21:43:41 +0200

    move find_package assimp to examples

--- examples/LibConfig.cmake
+++ examples/LibConfig.cmake
@@ -1,5 +1,6 @@
 find_package(imgui CONFIG)
 find_package(Bullet CONFIG)
+find_package(assimp CONFIG QUIET)
 find_package(nlohmann_json CONFIG QUIET)
 
 if (BULLET_FOUND AND NOT TARGET Bullet::Bullet)
