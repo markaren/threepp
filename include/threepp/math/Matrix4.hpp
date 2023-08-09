259ff0ccea3d03c1ff54a04d313b1053062c5dd0
Lars Ivar Hatledal <laht@ntnu.no>
2023-08-09 14:08:09 +0200

    Add doc

--- include/threepp/math/Matrix4.hpp
+++ include/threepp/math/Matrix4.hpp
@@ -13,6 +13,7 @@ namespace threepp {
     class Quaternion;
     class Matrix3;
 
+    // A class representing a 4x4 matrix.
     class Matrix4 {
 
     public:
@@ -26,14 +27,19 @@ namespace threepp {
 
         float& operator[](unsigned int index);
 
+        // Set the elements of this matrix to the supplied row-major values n11, n12, ... n44.
         Matrix4& set(float n11, float n12, float n13, float n14, float n21, float n22, float n23, float n24, float n31, float n32, float n33, float n34, float n41, float n42, float n43, float n44);
 
+        // Resets this matrix to the identity matrix.
         Matrix4& identity();
 
+        // Copies the elements of matrix m into this matrix.
         Matrix4& copy(const Matrix4& m);
 
+        // Copies the translation component of the supplied matrix m into this matrix's translation component.
         Matrix4& copyPosition(const Matrix4& m);
 
+        // Set the upper 3x3 elements of this matrix to the values of the Matrix3 m.
         Matrix4& setFromMatrix3(const Matrix3& m);
 
         Matrix4& extractBasis(Vector3& xAxis, Vector3& yAxis, Vector3& zAxis);
@@ -46,38 +52,54 @@ namespace threepp {
 
         Matrix4& makeRotationFromQuaternion(const Quaternion& q);
 
+        // Constructs a rotation matrix, looking from eye towards target oriented by the up vector.
         Matrix4& lookAt(const Vector3& eye, const Vector3& target, const Vector3& up);
 
+        // Post-multiplies this matrix by m.
         Matrix4& multiply(const Matrix4& m);
 
+        // Pre-multiplies this matrix by m.
         Matrix4& premultiply(const Matrix4& m);
 
+        // Sets this matrix to a x b.
         Matrix4& multiplyMatrices(const Matrix4& a, const Matrix4& b);
 
+        // Multiplies every component of the matrix by a scalar value s.
         Matrix4& multiplyScalar(float s);
 
+        // Computes and returns the determinant of this matrix.
         [[nodiscard]] float determinant() const;
 
         Matrix4& transpose();
 
+        // Sets the position component for this matrix from vector v, without affecting the rest of the matrix.
         Matrix4& setPosition(const Vector3& v);
 
+        // Sets the position component for this matrix from x, y, z, without affecting the rest of the matrix.
         Matrix4& setPosition(float x, float y, float z);
 
+        // Inverts this matrix, using the analytic method. You can not invert with a determinant of zero. If you attempt this, the method produces a zero matrix instead.
         Matrix4& invert();
 
+        // Multiplies the columns of this matrix by vector v.
         Matrix4& scale(const Vector3& v);
 
+        // Gets the maximum scale value of the 3 axes.
         [[nodiscard]] float getMaxScaleOnAxis() const;
 
+        // Sets this matrix as a translation transform from the numbers x, y and z,
         Matrix4& makeTranslation(float x, float y, float z);
 
+        // Sets this matrix as a translation transform from vector the v,
         Matrix4& makeTranslation(const Vector3& v);
 
+        // Sets this matrix as a rotational transformation around the X axis by theta (θ) radians.
         Matrix4& makeRotationX(float theta);
 
+        // Sets this matrix as a rotational transformation around the Y axis by theta (θ) radians.
         Matrix4& makeRotationY(float theta);
 
+        // Sets this matrix as a rotational transformation around the Z axis by theta (θ) radians.
         Matrix4& makeRotationZ(float theta);
 
         Matrix4& makeRotationAxis(const Vector3& axis, float angle);
@@ -86,6 +108,7 @@ namespace threepp {
 
         Matrix4& makeShear(float xy, float xz, float yx, float yz, float zx, float zy);
 
+        // Sets this matrix to the transformation composed of position, quaternion and scale.
         Matrix4& compose(const Vector3& position, const Quaternion& quaternion, const Vector3& scale);
 
         Matrix4& decompose(Vector3& position, Quaternion& quaternion, Vector3& scale);
