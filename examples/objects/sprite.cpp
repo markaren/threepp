b0c1aaccde21dea4ebc55cf666b2242567c3475f
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2023-10-06 20:58:12 +0200

    improve sprites example

--- examples/objects/sprite.cpp
+++ examples/objects/sprite.cpp
@@ -6,38 +6,56 @@ using namespace std::string_literals;
 
 namespace {
 
-    void setupHud(Scene& hudScene, WindowSize size) {
+    auto createHudSprites( WindowSize size) {
         TextureLoader tl;
         auto hudMaterial = SpriteMaterial::create();
         hudMaterial->map = tl.load("data/textures/sprite0.png");
-        hudMaterial->sizeAttenuation = false;
         hudMaterial->map->offset.set(0.5, 0.5);
+
+        auto imageWidth = static_cast<float>(hudMaterial->map->image->width);
+        auto imageHeight = static_cast<float>(hudMaterial->map->image->height);
 
         auto hudSprite1 = Sprite::create(hudMaterial);
         hudSprite1->center.set(0, 1);
-        hudSprite1->scale.set(hudMaterial->map->image->width / 2, hudMaterial->map->image->height / 2, 1);
+        hudSprite1->scale.set(imageWidth / 2, imageHeight / 2, 1);
 
         auto hudSprite2 = Sprite::create(hudMaterial);
         hudSprite2->center.set(1, 1);
-        hudSprite2->scale.set(hudMaterial->map->image->width / 2, hudMaterial->map->image->height / 2, 1);
+        hudSprite2->scale.set(imageWidth / 2, imageHeight / 2, 1);
 
         auto hudSprite3 = Sprite::create(hudMaterial);
         hudSprite3->center.set(0, 0);
-        hudSprite3->scale.set(hudMaterial->map->image->width / 2, hudMaterial->map->image->height / 2, 1);
+        hudSprite3->scale.set(imageWidth / 2, imageHeight / 2, 1);
 
         auto hudSprite4 = Sprite::create(hudMaterial);
         hudSprite4->center.set(1, 0);
-        hudSprite4->scale.set(hudMaterial->map->image->width / 2, hudMaterial->map->image->height / 2, 1);
+        hudSprite4->scale.set(imageWidth / 2, imageHeight / 2, 1);
 
         hudSprite1->position.set(-size.width / 2, size.height / 2, 1); // top left
         hudSprite2->position.set(size.width / 2, size.height / 2, 1);  // top right
         hudSprite3->position.set(-size.width / 2, -size.height / 2, 1);// bottom left
         hudSprite4->position.set(size.width / 2, -size.height / 2, 1); // bottom right
 
-        hudScene.add(hudSprite1);
-        hudScene.add(hudSprite2);
-        hudScene.add(hudSprite3);
-        hudScene.add(hudSprite4);
+        auto group = Group::create();
+        group->add(hudSprite1);
+        group->add(hudSprite2);
+        group->add(hudSprite3);
+        group->add(hudSprite4);
+
+        return group;
+    }
+
+    auto createSprites(const std::shared_ptr<SpriteMaterial>& material) {
+        auto sprites = Group::create();
+        for (int x = -4; x <= 4; x++) {
+            for (int y = -4; y <= 4; y++) {
+                auto sprite = Sprite::create(material);
+                sprite->position.x = static_cast<float>(x);
+                sprite->position.y = static_cast<float>(y);
+                sprites->add(sprite);
+            }
+        }
+        return sprites;
     }
 
 }// namespace
@@ -65,18 +83,11 @@ int main() {
     material->map = loader.load("data/textures/three.png");
     material->map->offset.set(0.5, 0.5);
 
-    auto sprites = Group::create();
-    for (int x = -4; x <= 4; x++) {
-        for (int y = -4; y <= 4; y++) {
-            auto sprite = Sprite::create(material);
-            sprite->position.x = static_cast<float>(x);
-            sprite->position.y = static_cast<float>(y);
-            sprites->add(sprite);
-        }
-    }
-    scene->add(sprites);
+    auto pickMaterial = material->clone()->as<SpriteMaterial>();
 
-    setupHud(*hudScene, size);
+    auto sprites = createSprites(material);
+    scene->add(sprites);
+    hudScene->add(createHudSprites(size));
 
     auto helper = Mesh::create(SphereGeometry::create(0.1));
     scene->add(helper);
@@ -97,16 +108,26 @@ int main() {
 
     Clock clock;
     Raycaster raycaster;
+    Sprite* lastPicked = nullptr;
     canvas.animate([&]() {
+        if (lastPicked) {
+            lastPicked->material = material;
+            lastPicked->scale.set(1, 1, 1);
+        }
+
         helper->visible = false;
         material->rotation += 1 * clock.getDelta();
 
         raycaster.setFromCamera(mouse, *camera);
-        auto intersects = raycaster.intersectObject(*sprites, true);
+        auto intersects = raycaster.intersectObjects(sprites->children, true);
         if (!intersects.empty()) {
-            auto& i = intersects.front();
-            helper->position.copy(i.point);
+            auto& intersection = intersects.front();
+            helper->position.copy(intersection.point);
             helper->visible = true;
+
+            lastPicked = intersection.object->as<Sprite>();
+            lastPicked->material = pickMaterial;
+            lastPicked->scale.set(1.2, 1.2, 1.2);
         }
 
         renderer.clear();
