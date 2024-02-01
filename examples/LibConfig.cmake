660d9dccf7a0d47acee80c7ea5250e0229e340bf
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2024-02-01 19:15:05 +0100

    update LibConfig.cmake

--- examples/LibConfig.cmake
+++ examples/LibConfig.cmake
@@ -1,20 +1,6 @@
-find_package(imgui CONFIG)
+find_package(imgui CONFIG QUIET)
 find_package(assimp CONFIG QUIET)
-find_package(pugixml CONFIG QUIET)
-
-find_path(MINIAUDIO_INCLUDE_DIRS "miniaudio.h")
 
 if (imgui_FOUND)
-
-    if (EXISTS "${CMAKE_BINARY_DIR}/_deps/imgui_glfw")
-        set(imgui_bindings "${CMAKE_BINARY_DIR}/_deps/imgui_glfw")
-        add_library(imgui_glfw "${imgui_bindings}/imgui_impl_glfw.cpp" "${imgui_bindings}/imgui_impl_opengl3.cpp")
-        target_link_libraries(imgui_glfw PUBLIC glad::glad glfw::glfw imgui::imgui)
-        target_include_directories(imgui_glfw PUBLIC "${imgui_bindings}")
-
-        set_property(TARGET imgui::imgui APPEND PROPERTY INTERFACE_LINK_LIBRARIES imgui_glfw)
-    endif ()
-
     set_property(TARGET imgui::imgui APPEND PROPERTY INTERFACE_COMPILE_DEFINITIONS HAS_IMGUI)
-
 endif ()
