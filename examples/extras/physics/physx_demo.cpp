4762827b07c9c5a63c079b52af95376605067400
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2024-03-13 09:59:46 +0100

    update [skip ci]

--- examples/extras/physics/physx_demo.cpp
+++ examples/extras/physics/physx_demo.cpp
@@ -68,6 +68,7 @@ namespace {
         auto material = MeshPhongMaterial::create();
 
         auto mesh = Mesh::create(geometry, material);
+        mesh->castShadow = true;
 
         RigidBodyInfo info;
         info.setMass(10);
@@ -82,6 +83,7 @@ int main() {
 
     Canvas canvas("PhysX", {{"aa", 6}});
     GLRenderer renderer(canvas.size());
+    renderer.shadowMap().enabled = true;
 
     Scene scene;
     scene.background = Color::aliceblue;
@@ -95,14 +97,17 @@ int main() {
 
     auto box1 = Mesh::create(BoxGeometry::create(0.5, 1, 0.5), defaultMaterial);
     box1->position.y = 0.5;
+    box1->castShadow = true;
     scene.add(box1);
 
     auto box2 = Mesh::create(BoxGeometry::create(0.25, 2, 0.25), defaultMaterial);
     box2->position.y = 1.5;
+    box2->castShadow = true;
     box1->add(box2);
 
     auto box3 = Mesh::create(CylinderGeometry::create(0.15, 0.15, 0.5), defaultMaterial);
     box3->position.y = 1.25;
+    box3->castShadow = true;
     box2->add(box3);
 
     STLLoader loader;
@@ -110,17 +115,26 @@ int main() {
     geometry->scale(5, 5, 5);
     auto sphereMaterial = MeshPhongMaterial::create({{"color", Color::red}});
     auto mesh = Mesh::create(geometry, sphereMaterial);
+    mesh->castShadow = true;
     mesh->position.y = 10;
     scene.add(mesh);
 
     auto groundMaterial = MeshPhongMaterial::create({{"color", Color::gray}});
     auto ground = Mesh::create(BoxGeometry::create(60, 0.1, 60), groundMaterial);
     ground->position.y = -0.05;
+    ground->receiveShadow = true;
     scene.add(ground);
 
-    auto light1 = HemisphereLight::create();
-    light1->position.y = 5;
+    auto light1 = AmbientLight::create(Color::white, 0.3f);
     scene.add(light1);
+
+    auto light2 = DirectionalLight::create();
+    light2->position.set(100, 100, -100);
+    light2->castShadow = true;
+    auto shadowCamera = light2->shadow->camera->as<OrthographicCamera>();
+    shadowCamera->left = shadowCamera->bottom = -10;
+    shadowCamera->right = shadowCamera->top = 10;
+    scene.add(light2);
 
     PxEngine engine;
 
