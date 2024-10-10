8cb9c0ff156e61cbddabe0399af6d1a061e0a946
Lars Ivar Hatledal <laht@ntnu.no>
2024-10-10 11:22:18 +0200

    rework DepthTexture changes

--- include/threepp/textures/DataTexture.hpp
+++ include/threepp/textures/DataTexture.hpp
@@ -6,31 +6,31 @@
 
 namespace threepp {
 
-    template<class T = unsigned char>
     class DataTexture: public Texture {
 
     public:
-        void setData(const std::vector<T>& data) {
+        void setData(const ImageData& data) {
 
             image().setData(data);
         }
 
+        template<class T = unsigned char>
         static std::shared_ptr<DataTexture> create(
                 int channels,
                 unsigned int width, unsigned int height) {
 
-            return std::shared_ptr<DataTexture>(new DataTexture(width*height*channels, width, height));
+            return std::shared_ptr<DataTexture>(new DataTexture(std::vector<T>(width * height * channels), width, height));
         }
 
         static std::shared_ptr<DataTexture> create(
-                const std::vector<T>& data,
+                const ImageData& data,
                 unsigned int width, unsigned int height) {
 
             return std::shared_ptr<DataTexture>(new DataTexture(data, width, height));
         }
 
     private:
-        explicit DataTexture(std::vector<T> data, unsigned int width, unsigned int height)
+        explicit DataTexture(ImageData data, unsigned int width, unsigned int height)
             : Texture({Image(std::move(data), width, height)}) {
             this->magFilter = Filter::Nearest;
             this->minFilter = Filter::Nearest;
@@ -40,14 +40,7 @@ namespace threepp {
 
             this->needsUpdate();
         }
-
-        explicit DataTexture(size_t size, unsigned int width, unsigned int height)
-            : DataTexture(std::vector<T>(size), width, height) {}
     };
-
-    extern template class DataTexture<unsigned char>;
-    extern template class DataTexture<float>;
-
 
 }// namespace threepp
 
