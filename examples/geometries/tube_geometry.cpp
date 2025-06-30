8c7d229e6ddb41fadc74500aabaa7a822fb05ed2
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2025-06-30 13:10:21 +0200

    update tube example [skip ci]

--- examples/geometries/tube_geometry.cpp
+++ examples/geometries/tube_geometry.cpp
@@ -71,18 +71,25 @@ int main() {
 
     OrbitControls controls{*camera, canvas};
 
-    auto tube = makeTubeMesh();
-    const auto geometry = tube->geometry();
-    scene->add(tube);
+    auto tube1 = makeTubeMesh();
+    const auto geometry1 = tube1->geometry();
+    scene->add(tube1);
 
-    BVH bvh;
-    bvh.build(*tube->geometry());
+    BVH bvh1;
+    bvh1.build(*geometry1);
 
     std::vector<BVHBox3> boxes;
-    bvh.collectBoxes(boxes);
+    bvh1.collectBoxes(boxes);
 
     auto boxesGroup = makeBoxes(boxes);
-    tube->add(boxesGroup);
+    tube1->add(boxesGroup);
+
+    auto tube2 = makeTubeMesh();
+    tube2->position.x = 10;
+    scene->add(tube2);
+
+    BVH bvh2;
+    bvh2.build(*tube2->geometry());
 
     KeyAdapter keyAdapter(KeyAdapter::Mode::KEY_PRESSED, [&](const KeyEvent& evt) {
         static bool show = boxesGroup->visible;
@@ -101,13 +108,33 @@ int main() {
         renderer.setSize(size);
     });
 
+    auto collisions = InstancedMesh::create(
+            SphereGeometry::create(0.5f, 16, 8),
+            MeshBasicMaterial::create({{"color", Color::yellow}}), 10000);
+    collisions->frustumCulled = false;// Disable frustum culling for collisions
+    scene->add(collisions);
+
+
     Clock clock;
-    const auto count = static_cast<float>(geometry->getIndex()->count());
+    const auto count = static_cast<float>(geometry1->getIndex()->count());
     canvas.animate([&]() {
-        tube->rotation.y += 1 * clock.getDelta();
+        tube1->rotation.y += 1 * clock.getDelta();
 
         const auto map = math::mapLinear(std::sin(clock.elapsedTime), -1, 1, 0, count);
-        geometry->setDrawRange(0, static_cast<int>(map));
+        geometry1->setDrawRange(0, static_cast<int>(map));
+
+        if (BVH::intersects(bvh1, bvh2, *tube1->matrixWorld, *tube2->matrixWorld)) {
+            static Matrix4 m;
+            const auto res = BVH::intersect(bvh1, *tube1->matrixWorld, bvh2, *tube2->matrixWorld);
+            collisions->setCount(res.size());
+            for (int i = 0; i < res.size(); ++i) {
+                const auto& intersection = res[i];
+                collisions->setMatrixAt(i, m.makeTranslation(intersection.position));
+            }
+            collisions->instanceMatrix()->needsUpdate();
+        } else {
+            collisions->setCount(0);
+        }
 
         renderer.render(*scene, *camera);
     });
