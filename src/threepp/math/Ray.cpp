589c19e298814b3c32f009c98c076d40936f1e98
Lars Ivar Hatledal <laht@ntnu.no>
2025-09-25 13:30:04 +0200

    make use of addScaledVector

--- src/threepp/math/Ray.cpp
+++ src/threepp/math/Ray.cpp
@@ -44,7 +44,7 @@ Ray& Ray::copy(const Ray& ray) {
 
 Vector3& Ray::at(float t, Vector3& target) const {
 
-    return target.copy(this->direction).multiplyScalar(t).add(this->origin);
+    return target.copy(this->origin).addScaledVector(this->direction, t);
 }
 
 Ray& Ray::lookAt(const Vector3& v) {
@@ -73,7 +73,7 @@ void Ray::closestPointToPoint(const Vector3& point, Vector3& target) const {
 
     } else {
 
-        target.copy(this->direction).multiplyScalar(directionDistance).add(this->origin);
+        target.copy(this->origin).addScaledVector(this->direction, directionDistance);
     }
 }
 
@@ -94,7 +94,7 @@ float Ray::distanceSqToPoint(const Vector3& point) const {
     }
 
 
-    _vector.copy(this->direction).multiplyScalar(directionDistance).add(this->origin);
+    _vector.copy(this->origin).addScaledVector(this->direction, directionDistance);
 
     return _vector.distanceToSquared(point);
 }
