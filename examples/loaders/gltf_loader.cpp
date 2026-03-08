b9036b33420d0109fca74989150a9efca9ad7246
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2026-03-08 15:45:28 +0100

    Add GLTFLoader

---
+++ examples/loaders/gltf_loader.cpp
@@ -0,0 +1,46 @@
+
+#include "threepp/loaders/GLTFLoader.hpp"
+#include "threepp/threepp.hpp"
+
+#include <iostream>
+
+using namespace threepp;
+
+int main() {
+    Canvas canvas("GLTF Demo");
+    GLRenderer renderer{canvas.size()};
+    renderer.outputEncoding = Encoding::sRGB;
+    renderer.shadowMap().enabled = true;
+
+    auto scene  = Scene::create();
+    scene->background = Color::aliceblue;
+    auto camera = PerspectiveCamera::create(60, canvas.aspect(), 0.1f, 100.f);
+    camera->position.set(0, 2, -4);
+
+    OrbitControls controls{*camera, canvas};
+
+    auto ambientLight = AmbientLight::create(0xffffff, 0.2f);
+    scene->add(ambientLight);
+
+    auto dirLight = DirectionalLight::create(0xffffff, 1.0f);
+    dirLight->position.set(1, 1, -1);
+    scene->add(dirLight);
+
+    GLTFLoader loader;
+    auto result = loader.load(std::string(DATA_FOLDER) + "/models/gltf/Soldier.glb");
+
+    if (!result) {
+        std::cerr << "Failed to load model\n";
+        return 1;
+    }
+
+    scene->add(result->scene);
+
+
+    // ---- Render loop ----
+    canvas.animate([&] {
+        renderer.render(*scene, *camera);
+    });
+
+    return 0;
+}
