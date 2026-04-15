d7b7ebd169ee31e4b7396fb67e58b080746ffdb0
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2026-04-15 21:32:06 +0200

    no versioning [skip ci]

--- build_rtdetr.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" >nul
-cmake --build C:/dev/threepp/cmake-build-release --target rtdetr_inference
