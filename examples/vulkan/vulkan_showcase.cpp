56efbe9621c35afd5003c5205d9e04576df0f0d7
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2026-05-15 01:29:21 +0200

    vulkan - ReSTIR DI + GI toggles in showcase ImGui panel [skip ci]

--- examples/vulkan/vulkan_showcase.cpp
+++ examples/vulkan/vulkan_showcase.cpp
@@ -273,6 +273,13 @@ int main() {
         if (ImGui::Combo("Tone mapping", &toneMode, toneItems, IM_ARRAYSIZE(toneItems)))
             renderer.toneMapping = static_cast<ToneMapping>(toneMode);
 
+        bool restirDI = renderer.restirDIEnabled();
+        if (ImGui::Checkbox("ReSTIR DI", &restirDI))
+            renderer.setRestirDIEnabled(restirDI);
+        bool restirGI = renderer.restirGIEnabled();
+        if (ImGui::Checkbox("ReSTIR GI", &restirGI))
+            renderer.setRestirGIEnabled(restirGI);
+
         if (ImGui::SliderInt("Samples / pixel", &spp, 1, 16))
             renderer.setSamplesPerPixel(spp);
 
