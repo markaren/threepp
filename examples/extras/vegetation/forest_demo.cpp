6cf5d9efab5bd09abf4ceadb64d21166741fec9f
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2026-06-16 20:27:27 +0200

    forest_demo - switch tonemapping [skip ci]

--- examples/extras/vegetation/forest_demo.cpp
+++ examples/extras/vegetation/forest_demo.cpp
@@ -489,7 +489,7 @@ int main() {
 #endif
 
     renderer->setClearColor(Color(0.62f, 0.72f, 0.84f));
-    renderer->toneMapping = ToneMapping::ACESFilmic;
+    renderer->toneMapping = ToneMapping::Neutral;
     renderer->toneMappingExposure = 1.0f;
     renderer->shadowMap().enabled = true;
     renderer->shadowMap().type = ShadowMap::PFCSoft;
